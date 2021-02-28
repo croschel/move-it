af383aa694257bf6f5c919e699042dfe178a82a8
Caique Roschel <croschel000@gmail.com>
2021-02-28 12:06:19 -0300

    change minutes for rest

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -25,7 +25,7 @@ export const CountdownContext = createContext({} as CountdownContextData);
 export function CountdownProvider({ children }: CountdownContextProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(45 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
